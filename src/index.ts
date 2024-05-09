151193dd8f044fe1fd3c3c0193a7b20df79ac90e
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-05-10 00:11:28 +0900

    add: print only the payload.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 
 function main() {
   console.log(sum(1, 2));
-  axios.get("https://example.com").then(console.log);
+  axios.get("https://example.com").then((r) => {
+    console.log(r.data);
+  });
 }
 
 main();
